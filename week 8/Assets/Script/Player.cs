1a446ee287c94035ed322767efcbbc0dbb25b8f6
cleon0079 <69292383+cleon0079@users.noreply.github.com>
2023-09-27 15:18:32 +1000

    finial update

--- week 8/Assets/Script/Player.cs
+++ week 8/Assets/Script/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 10;
+    [SerializeField] private float rotationSpeed = 30;
 
     // Start is called before the first frame update
     void Start()
@@ -15,14 +16,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movment();
 
-        
+
     }
 
-    private void movment() {
-        if (Input.GetKeyDown(KeyCode.W))
+    private void Movment() {
+        if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector3.forward * speed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up * rotationSpeed  * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.Self);
         }
     }
 }
